fe0e12ee6acebae174830f00829afe6210922760
Sang-Hun <rtg951118@gmail.com>
2020-04-23 12:07:13 +0900

    commit-sanghun

--- src/main/java/com/douzone/codingvirus19/Handler/SocketHandler.java
+++ src/main/java/com/douzone/codingvirus19/Handler/SocketHandler.java
@@ -1,18 +1,46 @@
 package com.douzone.codingvirus19.Handler;
 
+import java.util.HashMap;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+// 밑의 @Component 어노테이션을 통해서 Class를 Bean으로 등록 해 줌
+@Component
 public class SocketHandler extends TextWebSocketHandler {
 
+	HashMap<String, WebSocketSession> sessionMap = new HashMap<>(); //웹소켓 세션을 담아둘 해시맵
+	
 	@Override
-	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		String payload = message.getPayload();
-		System.out.println(payload);
-		System.out.println("message");
-		session.sendMessage(new TextMessage("Hi " + payload + " how may we help you?"));
+	public void handleTextMessage(WebSocketSession session, TextMessage message) {
+		//메시지 발송
+		String msg = message.getPayload();
+		for(String key : sessionMap.keySet()) {
+			//sessionMap.keySet()은 Map의 전체 값을 뽑아내기 위함 즉, sessionMap의 값만 뽑아내서 WebSocketSession에 담음.
+			session = sessionMap.get(key);
+			try {
+				// 담은 세션에 msg를 담음 msg는 JS에서 보낸 (이름이나 채팅 내용 등, 전체 내용)String을 session을 통해 보냄
+				session.sendMessage(new TextMessage(msg));
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
-
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		// 소켓 연결
+		// Map에 session의 id와 시작 session 담기
+		sessionMap.put(session.getId(), session);
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		// 소켓 종료
+		// Map에 session의 session 취소 시키기
+		sessionMap.remove(session.getId());
+	}
 }
--- src/main/java/com/douzone/codingvirus19/config/WebSocketConfig.java
+++ src/main/java/com/douzone/codingvirus19/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.douzone.codingvirus19.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -10,10 +11,16 @@ import com.douzone.codingvirus19.Handler.SocketHandler;
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
-
+	
+	@Autowired
+	SocketHandler socketHandler;
+	// Bean을 자동으로 주입하기 위해서 사용하는데, socketHandler와 "/chatting"경로를 addHandler로 담는다.
+	// 위의 방식의 경우 @Component 및 @Bean registry.addHandler의 경로를 쉽게 받을 수 있다.
+	
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(new SocketHandler(), "/user");
+		registry.addHandler(socketHandler, "/chatting");
+//		registry.addHandler(socketHandler, "/Editor");
 	}
 
 }
