166fb02f791ccef9ba878015b717b283e548c35c
Sang-Hun <rtg951118@gmail.com>
2020-05-20 15:45:45 +0900

    ㅇㄹㅇ

---
+++ front/react/src/contents/chatting/chatroomList.js
@@ -0,0 +1,29 @@
+import ChatRoom from './chatroom';
+import React, { Fragment } from "react";
+import Card from 'react-bootstrap/Card'
+import Accordion from 'react-bootstrap/Accordion'
+
+import styles from "./chatroomList.css";
+export default class ChatRoomList extends React.Component {
+    render() {
+        return (
+            <Card>
+                {this.props.contents.map(content => {
+                    return (
+                        <Fragment key={ content.group_no }>
+                            <Accordion.Toggle className={styles.card__header} as={Card.Header} eventKey={content.group_no} >
+                                <p>그룹{content.group_no}</p>
+                            </Accordion.Toggle>
+                            <Accordion.Collapse eventKey={content.group_no}>
+                                <Fragment>
+                                    <ChatRoom group_no={content.group_no} chatMessages={content.contents} />
+                                </Fragment>
+                            </Accordion.Collapse>
+
+                        </Fragment>
+                    )
+                })}
+            </Card>
+        )
+    }
+}
