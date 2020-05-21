b5f30bc216ea363f68d23840121ff461d9b9a280
BIT <swj03322@naver.com>
2020-05-21 10:51:33 +0900

    css 수정중

--- front/react/src/Container.js
+++ front/react/src/Container.js
@@ -95,7 +95,7 @@ export default class Container extends React.Component {
 
   render() {
     return (
-      <div className="container">
+      <div className={styles.container}>
         <Header />
         <Sidebar group={this.state.group} group_update={this.SidebarGroupUpdate.bind(this)} />
         <Contents />
