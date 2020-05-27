68f5e24804bdadf087e878ff0493d0baa19ca0e0
JUNG JUN YONG <jkouese@gmail.com>
2020-05-27 10:56:04 +0900

    contents 수정

--- front/react/src/contents/Contents.js
+++ front/react/src/contents/Contents.js
@@ -2,6 +2,7 @@ import React from "react";
 import ContentsHeader from "./ContentsHeader";
 import ContentsMemo from "./ContentsMemo";
 import Footer from "../footer/Footer";
+import Chat from "./Chat";
 import styles from "./Contents.css";
 
 export default class Contents extends React.Component {
@@ -12,18 +13,21 @@ export default class Contents extends React.Component {
   render() {
     return (
       <div className={styles.contents}>
-
-        <ContentsHeader
-          groupBySidebar={this.props.groupBySidebar}
-          group={this.props.group}
-        />
-    
-        <ContentsMemo
-          groupBySidebar={this.props.groupBySidebar}
-          group={this.props.group}
-          memo_bigArr={this.props.memo_bigArr}
-        />
-        <Footer />
+        <div className={styles.box}>
+          <div className={styles.contentsMemo}>
+            <ContentsHeader
+              groupBySidebar={this.props.groupBySidebar}
+              group={this.props.group}
+            />
+            <ContentsMemo
+              groupBySidebar={this.props.groupBySidebar}
+              group={this.props.group}
+              memo_bigArr={this.props.memo_bigArr}
+            />
+          </div>
+          <Chat />
+        </div>
+        {/* <Footer /> */}
       </div>
     );
   }
