fb567463699249cf1a0ee37bec9215cc93b8307f
kin911014 <50545361+kin911014@users.noreply.github.com>
2020-06-02 11:04:07 +0900

    download file to local

--- front/react/src/contents/toolbar/Toolbar.js
+++ front/react/src/contents/toolbar/Toolbar.js
@@ -41,6 +41,7 @@ export default class Toolbar extends React.PureComponent {
           group={this.props.group}
           clientRef={this.props.clientRef}
           users={this.props.users}
+          // memo 색변경 시 toolbar의 버튼도 색변경을 도와주는 props
           setStyle={this.props.setStyle}
         />
 
@@ -54,32 +55,44 @@ export default class Toolbar extends React.PureComponent {
           // gName,gNo : 임의의 작동을 한 그룹의 no와 name으로 콜백이동하기위한 props
           gName={this.props.groupBySidebar.name}
           gNo={this.props.groupBySidebar.no}
+          // memo 색변경 시 toolbar의 버튼도 색변경을 도와주는 props
           setStyle={this.props.setStyle}
         />
 
         {/* 해시추가 */}
         <AddHash
           hash={this.props.hash}
+          // 내가 클릭한 메모의 color를 가져온다.
           no={this.props.no}
           memo_gNo={this.state.memo_gNo}
           memo_hash={this.props.memo_hash}
+          // memo 색변경 시 toolbar의 버튼도 색변경을 도와주는 props
           setStyle={this.props.setStyle}
         />
 
         {/* 내 컴퓨터에 저장 */}
-        <SaveLocal setStyle={this.props.setStyle} />
+        <SaveLocal
+          gName={this.props.groupBySidebar.name}
+          // SaveLocal에서 저장시킬 contents값
+          content={this.props.content}
+          // memo 색변경 시 toolbar의 버튼도 색변경을 도와주는 props
+          setStyle={this.props.setStyle}
+        />
 
         {/* 외부공유 */}
         <ExternalSharing setStyle={this.props.setStyle} />
 
         {/* 메모삭제 */}
         <MemoDelete
-          setStyle={this.props.setStyle}
           // SidebarGroupUpdate: delete클릭 후 수정된 메모list를 다시뿌려주기 위한 callback함수
           SidebarGroupUpdate={this.props.SidebarGroupUpdate}
+          // 내가 클릭한 메모의 color를 가져온다.
           no={this.props.no}
+          // gName,gNo : 임의의 작동을 한 그룹의 no와 name으로 콜백이동하기위한 props
           gName={this.props.groupBySidebar.name}
           gNo={this.props.groupBySidebar.no}
+          // memo 색변경 시 toolbar의 버튼도 색변경을 도와주는 props
+          setStyle={this.props.setStyle}
         />
       </div>
     );
