f3a6b67fbc40eec38e9a8230e70bd521945154ac
kin911014 <50545361+kin911014@users.noreply.github.com>
2020-06-05 15:21:08 +0900

    working chat css #3
    message button

--- front/react/src/contents/chatting/messageSend.js
+++ front/react/src/contents/chatting/messageSend.js
@@ -1,51 +1,70 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
-import styles from './messageSend.css';
+import styles from "./messageSend.css";
 
-export default class MessageList extends React.Component{
-
-    constructor() {
-        super();
-        this.state = {
-           message: '',
-           //messageBox:''
-        }
-        this.onChatSubmit = this.onChatSubmit.bind(this);
-        this.onInputChanged = this.onInputChanged.bind(this);
-     }
-
-    onInputChanged(event) {
-        this.setState({
-           message: event.target.value
-        });
-    }
-
-     chatKeyPress(event) {
-        if (event.key === "Enter") {
-           this.onChatSubmit(event);
-        }
+export default class MessageList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      message: "",
+      //messageBox:''
     };
+    this.onChatSubmit = this.onChatSubmit.bind(this);
+    this.onInputChanged = this.onInputChanged.bind(this);
+  }
 
-    onChatSubmit(event) {
-        event.preventDefault()
-        //this.state.messageBox.push(this.state.message);
-        this.props.sendMessage(this.state.message, this.props.gNo);
-        this.setState({
-            message: ''
-        })
-    }
+  onInputChanged(event) {
+    this.setState({
+      message: event.target.value,
+    });
+  }
 
-    render(){
-        return(
-            <form onSubmit={this.onChatSubmit.bind(this)} className={styles.inputTable}>
-                <input name='chat' type="text" className={styles.Message} placeholder="내용을 입력하세요"
-                    value={this.state.message}
-                    onChange={this.onInputChanged.bind(this)}
-                    onKeyPress={this.chatKeyPress.bind(this)} />
-                <button className={styles.button} onClick={this.onChatSubmit.bind(this)}> <FontAwesomeIcon className={styles.faPaperPlane} icon={faPaperPlane}/> </button>
-            </form>
-        )
+  chatKeyPress(event) {
+    if (event.key === "Enter") {
+      this.onChatSubmit(event);
     }
+  }
+
+  onChatSubmit(event) {
+    event.preventDefault();
+    //this.state.messageBox.push(this.state.message);
+    this.props.sendMessage(this.state.message, this.props.gNo);
+    this.setState({
+      message: "",
+    });
+  }
+
+  render() {
+    return (
+      <form
+        onSubmit={this.onChatSubmit.bind(this)}
+        className={styles.inputTable}
+      >
+        <div className={styles.MessageBox}>
+          <input
+            name="chat"
+            type="text"
+            className={styles.Message}
+            placeholder="내용을 입력하세요"
+            value={this.state.message}
+            onChange={this.onInputChanged.bind(this)}
+            onKeyPress={this.chatKeyPress.bind(this)}
+          />
+          <button
+            className={styles.button}
+            onClick={this.onChatSubmit.bind(this)}
+          >
+            {/* {" "}
+            <FontAwesomeIcon
+              className={styles.faPaperPlane}
+              icon={faPaperPlane} */}
+            {/* /> */}
+            전송
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
