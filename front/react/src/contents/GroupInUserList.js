1023168d815f1e2a585d2e5f2d692cf5148a6282
Sang-Hun <rtg951118@gmail.com>
2020-06-10 16:35:19 +0900

    t

--- front/react/src/contents/GroupInUserList.js
+++ front/react/src/contents/GroupInUserList.js
@@ -6,38 +6,54 @@ export default class GroupInUserList extends React.Component {
     constructor() {
         super(...arguments)
         this.state = {
+            userlistSession: [],
             groupInUserList: this.props.groupInUserList
         }
     }
-    
-    userSessionReceive(userSession){
-
+    userSessionReceive(userSession) {
+        console.log(userSession);
+        this.setState({
+            userlistSession: userSession
+        });
     }
 
     render() {
         console.log(this.props.users.no[0]);
-        const wsSourceUrl = "http://localhost:8080/codingvirus19/api/userList";
+        const wsSourceUrl = "./api/userlist";
         console.log(this.props.groupInUserList);
         return (
             <div className={styles.groupUserList}>
                 {this.props.groupInUserList.map((element) => {
                     if (this.props.groupInUserList != null) {
-                        return (
-                            <ul key={element.user_no} className={styles.users}>
-                                <button className={styles.userImage}>{element.img}</button>
-                                <span className={styles.userNotLogin}></span><br/>
-                                <li>{element.nickname}</li>
-                            </ul>
-                        );
+                        console.log(element.id,this.state.userlistSession.indexOf(element.id) == -1);
+                        if (this.state.userlistSession.indexOf(element.id) == -1) {
+                            return (
+                                <ul key={element.user_no} className={styles.users}>
+                                    <button className={styles.userImage}>{element.img}</button>
+                                    <span className={styles.userNotLogin}></span><br />
+                                    <li>{element.nickname}</li>
+                                </ul>
+                            );
+                        } else {
+                            return (
+                                <ul key={element.user_no} className={styles.users}>
+                                    <button className={styles.userImage}>{element.img}</button>
+                                    <span className={styles.userInLogin}></span><br />
+                                    <li>{element.nickname}</li>
+                                </ul>
+                            );
+                        }
+
                     }
                 })}
                 {this.props.users.no[0] != null ? (
-                <SockJsClient
-                    url={wsSourceUrl}
-                    topics={[`api/userList/${this.props.users.no[0]}`]}
-                    onMessage={this.userSessionReceive.bind(this)}
-                    ref={(client) => { this.clientRef = client; }}>
-                </SockJsClient>
+                    <SockJsClient
+                        url={wsSourceUrl}
+                        topics={[`/api/userlist/${this.props.users.no[0]}`]}
+                        onMessage={this.userSessionReceive.bind(this)}
+                        ref={(client) => { this.clientRef = client; }}
+                        onConnect={() => { this.clientRef.sendMessage(`/app/userlist/${this.props.users.no[0]}`) }}>
+                    </SockJsClient>
                 ) : null}
             </div>
         );
