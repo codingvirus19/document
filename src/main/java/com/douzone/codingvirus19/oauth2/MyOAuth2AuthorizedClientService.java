a80a62d2a11ff93ad3bc0d47554d30e9d290107d
JUNG JUN YONG <jkouese@gmail.com>
2020-07-08 10:31:44 +0900

    카카오 로그인 수정

--- src/main/java/com/douzone/codingvirus19/oauth2/MyOAuth2AuthorizedClientService.java
+++ src/main/java/com/douzone/codingvirus19/oauth2/MyOAuth2AuthorizedClientService.java
@@ -1,15 +1,9 @@
 package com.douzone.codingvirus19.oauth2;
 
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.jaas.SecurityContextLoginModule;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
